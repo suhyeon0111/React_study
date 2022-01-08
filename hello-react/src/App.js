598b26c3286908756074f2a4601e200026928b80
suhyeon <soo4833@naver.com>
2022-01-08 23:43:39 +0900

    [수정]: hello-react/src/App.js

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,23 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'suhyeon';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>React hello</h1>
+    <h2>Does it work well?</h2>
+    <p>my name is {name}</p>
+    </>
   );
 }
 
