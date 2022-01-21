cf67c666d49180367c13353ed2e791514b5c51ac
suhyeon <soo4833@naver.com>
2022-01-21 23:57:24 +0900

    [useState공부]

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
-// import MyComponent from "./MyComponent";
-import Counter from "./Counter";
+import Say from "./Say";
 
 const App = () => {
-  return <Counter />;
+  return <Say />;
 };
 
 export default App;
