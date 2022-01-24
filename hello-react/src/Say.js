02bcb51054ccce3fe55ceedd37427a664b08de05
suhyeon <soo4833@naver.com>
2022-01-24 23:57:01 +0900

    [useState 공부]

--- hello-react/src/Say.js
+++ hello-react/src/Say.js
@@ -5,11 +5,22 @@ const Say = () => {
   const onClickEnter = () => setMessage("안녕하세요!");
   const onClickLeave = () => setMessage("안녕히 가세요!");
 
+  const [color, setColor] = useState("black");
+
   return (
     <div>
       <button onClick={onClickEnter}>입장</button>
       <button onClick={onClickLeave}>퇴장</button>
       <h1>{message}</h1>
+      <button style={{ color: "red" }} onClick={() => setColor("red")}>
+        red
+      </button>
+      <button style={{ color: "blue" }} onClick={() => setColor("blue")}>
+        blue
+      </button>
+      <button style={{ color: "yellow" }} onClick={() => setColor("yellow")}>
+        yellow
+      </button>
     </div>
   );
 };
