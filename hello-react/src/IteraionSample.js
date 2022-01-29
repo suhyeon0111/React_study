1821e526f277e0ab734d9c6d4a8d8189b6ca26ef
suhyeon <soo4833@naver.com>
2022-01-29 23:50:45 +0900

    [데이터 추가 기능] concat -> 새로운 배열을 만들어 줌

--- hello-react/src/IteraionSample.js
+++ hello-react/src/IteraionSample.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
 const IterationSample = ()  => {
-    const names = ['봄', '여름', '가을', '겨울'];
-    const nameList = names.map((name,Index) => <li key={Index}>{name}</li>);
-    return <ul>{nameList}</ul>
+    const [names, setNames] = useState([
+        {id: 1, text: '봄'},
+        {id: 2, text: '여름'},
+        {id: 3, text: '가을'},
+        {id: 4, text: '겨울'}
+    ])
+    const [inputText, setInputText] = useState('');
+    const [naxtId, setNextId] = useState(5);
+    
+    const onChange = e => setInputText(e.target.value);
+    const onClick = () => {
+        const nextNames = names.concat({
+            id: setNextId,
+            text: inputText
+        });
+        setNextId(nextId + 1);
+        setNames(nextNames);
+        setInputText('');
+    }
+
+    const namesList = names.map(name => <li key = {name.id}>{name.text}</li>);
+    return (
+    <>
+    <input value={inputText} onChange={onChange} />
+    <button>추가</button>
+    <ul>{namesList}</ul>
+    </>
+    );
 }
 
 export default IterationSample;
