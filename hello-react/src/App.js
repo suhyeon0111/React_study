958b62e01387093cb83623b160b40b478abe3138
suhyeon <soo4833@naver.com>
2022-02-02 23:45:28 +0900

    [리렌더링 공부]

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,14 +1,25 @@
 import React, {Component} from "react";
-// import Say from "./Say";
-// import EventPractice from "./EventPractice";
-// import ValidationSample from "./ValidationSample";
-// import ScrollBox from "./ScrollBox";
-// import IterationSample from "./IteraionSample";
 import LifeCycleSample from "./LifeCycleSample";
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
 class App extends Component {
+  state = {
+    color: '#000000'
+  }
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    })
+  }
   render(){
     return (
-    <LifeCycleSample/>
+      <div>
+        <button onClick={this.handleClick}> 랜덤색상 </button>
+        <LifeCycleSample color={this.state.color}/>
+      </div>
     )
   }
 }
