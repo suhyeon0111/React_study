c6950b89b097c55780e49722f70ce43f3d466f87
suhyeon <soo4833@naver.com>
2022-02-04 04:53:52 +0900

    [리렌더링 공부] 에러발생시 사용자에게 에러가 발생했다는 것을 보여주기

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,26 +1,30 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import LifeCycleSample from "./LifeCycleSample";
+import ErrorBoundery from "./ErrorBoundery";
 
+// 렌덤 색상 생성 함수
 function getRandomColor() {
-  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
 }
 
 class App extends Component {
   state = {
-    color: '#000000'
-  }
+    color: "#000000",
+  };
   handleClick = () => {
     this.setState({
-      color: getRandomColor()
-    })
-  }
-  render(){
+      color: getRandomColor(),
+    });
+  };
+  render() {
     return (
       <div>
         <button onClick={this.handleClick}> 랜덤색상 </button>
-        <LifeCycleSample color={this.state.color}/>
+        <ErrorBoundery>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundery>
       </div>
-    )
+    );
   }
 }
 export default App;
