c40ab9b9bd3d9c6cba9fbcd6be652e2738b5bc8b
suhyeon <soo4833@naver.com>
2022-02-15 23:41:45 +0900

    [일정 관리 앱]: 데이터 전달 완료

--- todo-app/src/components/TodoListItem.js
+++ todo-app/src/components/TodoListItem.js
@@ -5,21 +5,22 @@ import {
   MdCheckBox,
   MdRemoveCircleOutline,
 } from 'react-icons/md';
-
+import cn from 'classnames';
 import './TodoListItem.scss';
 
-const TodoListItem = () => {
+const TodoListItem = ({ todo }) => {
+  const { text, checked } = todo;
+
   return (
-    <div backgroundcolor="black"></div>
-    // <div className="TodoListItem">
-    //   <div className="checkbox">
-    //     <MdCheckBoxOutlineBlank />
-    //     <div className="text">할 일</div>
-    //   </div>
-    //   <div className="remove">
-    //     <MdRemoveCircleOutline />
-    //   </div>
-    // </div>
+    <div className="TodoListItem">
+      <div className={cn('checkbox', { checked })}>
+        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+        <div className="text">{text}</div>
+      </div>
+      <div className="remove">
+        <MdRemoveCircleOutline />
+      </div>
+    </div>
   );
 };
 
