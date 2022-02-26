e79da39bdf6601a2fa9f8f8f17f1eeaa754baf7b
suhyeon <soo4833@naver.com>
2022-02-26 23:54:44 +0900

    [링크 추가]

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -13,10 +13,17 @@ const App = () => {
         <li>
           <Link to="/about">소개</Link>
         </li>
+        <li>
+          <Link to="/profile/velopert">velopert 프로필</Link>
+        </li>
+        <li>
+          <Link to="/profile/gildong">gildong 프로필</Link>
+        </li>
       </ul>
       <hr />
       <Route path="/" component={Home} exact={true} />
       <Route path={["/about", "/info"]} component={About} />
+      <Route path="/profile/:username" component={Profile} />
     </div>
   );
 };
