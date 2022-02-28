3a036def1fca6cedb827625228eccf7222d41be4
suhyeon <soo4833@naver.com>
2022-02-28 23:57:00 +0900

    [컴포넌트 구성하기]

---
+++ 3-component/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import AppTemplate from "./components/AppTemplate.js";
+import Header from "./components/Header.js";
+import "./App.css";
+
+const App = () => {
+  return <AppTemplate></AppTemplate>;
+};
+
+export default App;
---
+++ 3-component/src/components/AppTemplate.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./AppTemplate.scss";
+
+const AppTemplate = (children) => {
+  return <div className="container"></div>;
+};
+
+export default AppTemplate;
