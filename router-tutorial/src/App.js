4d9b6102dc5d0e95b39c7cb6bd7006c02dc8cbb5
suhyeon <soo4833@naver.com>
2022-03-04 23:53:04 +0900

    [라우터공부]

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,31 +1,49 @@
 import React from "react";
-import { Route, Link } from "react-router-dom";
+import { Route } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
 
 const App = () => {
   return (
     <div>
-      <ul>
-        <li>
-          <Link to="/">홈</Link>
-        </li>
-        <li>
-          <Link to="/about">소개</Link>
-        </li>
-        <li>
-          <Link to="/profile/velopert">velopert 프로필</Link>
-        </li>
-        <li>
-          <Link to="/profile/gildong">gildong 프로필</Link>
-        </li>
-      </ul>
-      <hr />
-      <Route path="/" component={Home} exact={true} />
-      <Route path={["/about", "/info"]} component={About} />
-      <Route path="/profile/:username" component={Profile} />
+      {/* <Route path="/" component={Home} />
+      <Route path="/about" component={About} /> */}
+      <About />
+      <Home />
     </div>
   );
 };
 
 export default App;
+
+// import React from "react";
+// import { Route, Link } from "react-router-dom";
+// import About from "./About";
+// import Home from "./Home";
+
+// const App = () => {
+//   return (
+//     <div>
+//       <ul>
+//         <li>
+//           <Link to="/">홈</Link>
+//         </li>
+//         <li>
+//           <Link to="/about">소개</Link>
+//         </li>
+//         <li>
+//           <Link to="/profile/velopert">velopert 프로필</Link>
+//         </li>
+//         <li>
+//           <Link to="/profile/gildong">gildong 프로필</Link>
+//         </li>
+//       </ul>
+//       <hr />
+//       <Route path="/" component={Home} exact={true} />
+//       <Route path={["/about", "/info"]} component={About} />
+//       <Route path="/profile/:username" component={Profile} />
+//     </div>
+//   );
+// };
+
+// export default App;
