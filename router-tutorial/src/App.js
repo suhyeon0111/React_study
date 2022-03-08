d75d31f5e945331383ab5b9ba0fdd32c8d43969a
suhyeon <soo4833@naver.com>
2022-03-08 23:52:59 +0900

    .

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,47 +1,31 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Link } from "react-router-dom";
 import About from "./components/About";
 import Home from "./components/Home";
+import Profile from "./components/Profile";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+        <li>
+          <Link to="/profiles">프로필</Link>
+        </li>
+      </ul>
+      <hr />
+      <Routes>
+        <Route path="/" element={<Home />} exact={true} />
+        <Route path={"/about"} element={<About />} />
+        <Route path="/profile/:username" element={Profile} />
+      </Routes>
+    </div>
   );
 };
 
 export default App;
-
-// import React from "react";
-// import { Route, Link } from "react-router-dom";
-// import About from "./About";
-// import Home from "./Home";
-
-// const App = () => {
-//   return (
-//     <div>
-//       <ul>
-//         <li>
-//           <Link to="/">홈</Link>
-//         </li>
-//         <li>
-//           <Link to="/about">소개</Link>
-//         </li>
-//         <li>
-//           <Link to="/profile/velopert">velopert 프로필</Link>
-//         </li>
-//         <li>
-//           <Link to="/profile/gildong">gildong 프로필</Link>
-//         </li>
-//       </ul>
-//       <hr />
-//       <Route path="/" component={Home} exact={true} />
-//       <Route path={["/about", "/info"]} component={About} />
-//       <Route path="/profile/:username" component={Profile} />
-//     </div>
-//   );
-// };
-
-// export default App;
--- router-tutorial/src/components/Profile.js
+++ router-tutorial/src/components/Profile.js
@@ -2,13 +2,12 @@ import React from "react";
 
 const data = {
   velopert: {
-    name: "이수현",
-    description: "리액트",
+    name: "김민준",
+    description: "리액트를 좋아하는 개발자",
   },
-
   gildong: {
-    name: "수현",
-    description: "공부시작",
+    name: "홍길동",
+    description: "고전 소설 홍길동전의 주인공",
   },
 };
 
@@ -16,7 +15,7 @@ const Profile = ({ match }) => {
   const { username } = match.params;
   const profile = data[username];
   if (!profile) {
-    return <div>존재하지 않는 사용자입니다. </div>;
+    return <div>존재하지 않는 사용자입니다.</div>;
   }
   return (
     <div>
