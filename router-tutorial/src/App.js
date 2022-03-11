1f865eb9ee696e42088749165dde03aaa9e5b9fc
suhyeon <soo4833@naver.com>
2022-03-11 22:39:36 +0900

    [수정 완료]

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,42 +1,10 @@
-// import React from "react";
-// import { Route, Routes, Link } from "react-router-dom";
-// import About from "./components/About";
-// import Home from "./components/Home";
-// import Profile from "./components/Profile";
-
-// const App = () => {
-//   return (
-//     <div>
-//       <ul>
-//         <li>
-//           <Link to="/">홈</Link>
-//         </li>
-//         <li>
-//           <Link to="/about">소개</Link>
-//         </li>
-//         <li>
-//           <Link to="/profiles">프로필</Link>
-//         </li>
-//       </ul>
-//       <hr />
-//       <Routes>
-//         <Route path="/" element={<Home />} exact={true} />
-//         <Route path={"/about"} element={<About />} />
-//         <Route path="/profile/:username" element={Profile} />
-//       </Routes>
-//     </div>
-//   );
-// };
-
-// export default App;
-
 import React from "react";
 import { Route, Link, Routes } from "react-router-dom";
 import About from "./components/About";
 import Home from "./components/Home";
 import Profile from "./components/Profile";
 
-function App() {
+const App = () => {
   return (
     <div>
       <ul>
@@ -57,10 +25,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/profile/:username" element={<Profile />} />
+        <Route path="/profile/:id" element={<Profile />} />
       </Routes>
     </div>
   );
-}
+};
 
 export default App;
--- router-tutorial/src/components/Profile.js
+++ router-tutorial/src/components/Profile.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const data = {
   velopert: {
@@ -11,20 +12,31 @@ const data = {
   },
 };
 
-const Profile = ({ match }) => {
-  const { username } = match.params;
-  const profile = data[username];
-  if (!profile) {
-    return <div>존재하지 않는 사용자입니다.</div>;
-  }
+const Profile = () => {
+  const { id } = useParams();
+  const profile = data[id];
+
   return (
     <div>
       <h3>
-        {username}({profile.name})
+        {id}({profile.name})
       </h3>
       <p>{profile.description}</p>
     </div>
   );
+  // const { username } = match.params;
+  // const profile = data[username];
+  // if (!profile) {
+  //   return <div>존재하지 않는 사용자입니다.</div>;
+  // }
+  // return (
+  //   <div>
+  //     <h3>
+  //       {username}({profile.name})
+  //     </h3>
+  //     <p>{profile.description}</p>
+  //   </div>
+  // );
 };
 
 export default Profile;
