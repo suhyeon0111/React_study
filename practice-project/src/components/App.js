7474ee607b15af253bba952344ca44ca4f3367e5
suhyeon0111 <soo4833@naver.com>
2022-11-30 23:48:25 +0900

    연습 용 파일

---
+++ practice-project/src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <div style={{ width: "100%", backgroundColor: "#f75690" }}></div>;
+    </>
+  );
+}
+
+export default App;
---
+++ practice-project/src/components/views/SearchPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function SearchPage() {
+  return (
+    <>
+      <div style={{ width: "100%", backgroundColor: "#f75690" }}></div>;
+    </>
+  );
+}
+export default SearchPage;
